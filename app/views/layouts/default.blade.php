2a0fd984786e90b58971789355f0ebf3c684fea8
Mike Ward <ward.michael51@gmail.com>
2015-05-26 23:28:17 -0400

    added meta viewport

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Forecastr</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/weather-icons.min.css') }}">

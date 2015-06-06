fa4419e2386f9b32ceeeccf0d6eb0ef321b5a8d7
Mike Ward <ward.michael51@gmail.com>
2015-06-05 23:59:21 -0400

    fixed the width issues of chartjs canvas

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -7,10 +7,10 @@
 	<title>Forecastr</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/weather-icons.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	{{ HTML::script('javascript/chartjs/Chart.min.js')}}
 	{{ HTML::script('javascript/themescripts.js')}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
 </head>
 <body>
